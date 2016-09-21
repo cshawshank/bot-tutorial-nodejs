283ac57e5ca65b21bfcbcd21f8b629828e0ccbd2
cshawshank <cshawshank@gmail.com>
2016-09-21 17:51:59 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ var HTTPS = require('https');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]), botRegex = /^@robotcasey$/;
+  var request = JSON.parse(this.req.chunks[0]);
+  var botRegex = /^@robotcasey$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
