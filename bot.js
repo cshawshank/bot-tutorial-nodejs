436b20a9b2f7118f00027a28ed8fde8790f385ca
cshawshank <cshawshank@gmail.com>
2016-12-29 18:24:56 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,24 @@ var google_api_key = process.env.GOOGLE_API_KEY;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
   
-  console.log("attachments: " +  JSON.stringify(request.attachments));
-  console.log("attachments length: " +  request.attachments.length);
+  console.log("text length: " +  request.text.length);
 
   if(request.sender_id != friend_id) {
     if(request.text && (request.text.indexOf("@robotcasey") != -1 || request.text.indexOf("@caseyrobot") != -1)) {
       this.res.writeHead(200);
-      postMessage("Sorry, I was only built to like (what i deem) postitive sentiment posts and all pictures.");
+      postMessage("Sorry, I was only built to like postitive posts and all attachments.");
       this.res.end();
     } else if(request.attachments.length > 0) {
       this.res.writeHead(200);
       likeMessage(request.group_id, request.id);
       this.res.end();
-    } else {
+    } else if(request.text && request.text.length > 0){
       this.res.writeHead(200);
       likeMessage(request.group_id, request.id);
+      this.res.end();
+    } else {
+      console.log("ignoring this request");
+      this.res.writeHead(200);
       this.res.end();
     }
   } else {
